7ddb24df362cdd3b4235bac27274146b00ffb147
myeongju-kim <mjoo1106@naver.com>
2022-10-26 11:26:59 +0900

    feat: add (ten) full lookup function

--- src/main/java/com/kgg/kart/search/service/SearchService.java
+++ src/main/java/com/kgg/kart/search/service/SearchService.java
@@ -2,8 +2,7 @@ package com.kgg.kart.search.service;
 
 import com.kgg.kart.common.dto.CommonResponse;
 import com.kgg.kart.search.domain.Search;
-import com.kgg.kart.search.dto.DecodeResponse;
-import com.kgg.kart.search.dto.SearchRequest;
+import com.kgg.kart.search.dto.*;
 import com.kgg.kart.search.exception.ApiException;
 import com.kgg.kart.search.repository.SearchRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -45,6 +47,32 @@ public class SearchService {
     }
     public CommonResponse sendNexon(SearchRequest searchRequest){
         String decode=saveInfo(searchRequest.getNickname());
+        RestTemplate restTemplate=new RestTemplate();
+        HttpHeaders headers=new HttpHeaders();
+        headers.set("Authorization", key);
+        HttpEntity request=new HttpEntity(headers);
+        String url=baseUrl+"/"+decode;
+        try{
+            ResponseEntity<MatchesResponse> result=restTemplate.exchange(url, HttpMethod.GET, request, MatchesResponse.class);
+            List<SearchResponse> searchResponses= new ArrayList<>();
+            tenSearchResponse tenSearchResponse=new tenSearchResponse();
+            for(MatchResponse match: result.getBody().getMatches()){
+                SearchResponse searchResponse=new SearchResponse();
+                searchResponse.setCharacter(match.getCharacter());
+                searchResponse.setEndTime(match.getEndTime());
+                searchResponse.setMatchResult(match.getMatchResult());
+                searchResponse.setMatchType(match.getMatchType());
+                searchResponse.setPlayerCount(match.getPlayerCount());
+                searchResponse.setStartTime(match.getStartTime());
+                searchResponse.setTrackId(match.getTrackId());
+                searchResponses.add(searchResponse);
+            }
+            tenSearchResponse.setSearchResponses(searchResponses);
+            commonResponse.setMessage("최근 10게임 전적 조회 결과입니다.");
+            commonResponse.setResult(tenSearchResponse);
+        }catch(Exception e) {
+            throw new ApiException("넥슨과 통신에 실패했습니다.");
+        }
         return commonResponse;
     }
 }
