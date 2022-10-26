fe6e8111caf74dd51b321bd2408ff81ce7a51f04
myeongju-kim <mjoo1106@naver.com>
2022-10-26 11:57:02 +0900

    fix: modify api request from get to post

--- src/main/java/com/kgg/kart/search/controller/SearchController.java
+++ src/main/java/com/kgg/kart/search/controller/SearchController.java
@@ -5,17 +5,14 @@ import com.kgg.kart.search.dto.SearchRequest;
 import com.kgg.kart.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class SearchController {
     private final SearchService searchService;
-    @RequestMapping(value="/api/v1/search", method = RequestMethod.GET)
-    public ResponseEntity<CommonResponse> scrapSave(@RequestParam SearchRequest searchRequest){
+    @RequestMapping(value="/api/v1/search", method = RequestMethod.POST) //DB에 저장해서 post
+    public ResponseEntity<CommonResponse> userSearch(@RequestBody SearchRequest searchRequest){
 
         return ResponseEntity.ok().body(searchService.sendNexon(searchRequest));
     }
