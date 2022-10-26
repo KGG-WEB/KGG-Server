f55f7a4993145ec6820bb8825e581f8999012d61
myeongju-kim <mjoo1106@naver.com>
2022-10-26 10:40:04 +0900

    feat: create search function

---
+++ src/main/java/com/kgg/kart/search/repository/SearchRepository.java
@@ -0,0 +1,7 @@
+package com.kgg.kart.search.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchRepository {
+}
---
+++ src/main/java/com/kgg/kart/search/service/SearchService.java
@@ -0,0 +1,7 @@
+package com.kgg.kart.search.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SearchService {
+}
