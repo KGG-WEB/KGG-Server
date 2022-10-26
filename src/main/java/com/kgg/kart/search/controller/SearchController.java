c8771a1c6233e8800f332a575668c92d51783da6
myeongju-kim <mjoo1106@naver.com>
2022-10-26 10:40:12 +0900

    feat: create search controller(api)

---
+++ src/main/java/com/kgg/kart/search/controller/SearchController.java
@@ -0,0 +1,10 @@
+package com.kgg.kart.search.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SearchController {
+
+}
