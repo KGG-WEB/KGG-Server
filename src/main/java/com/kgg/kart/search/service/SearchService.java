78badd582c3dfb89090ce31a8bf9119ed4e649bb
myeongju-kim <mjoo1106@naver.com>
2022-10-26 10:47:40 +0900

    feat: add nexon api connection code

--- src/main/java/com/kgg/kart/search/service/SearchService.java
+++ src/main/java/com/kgg/kart/search/service/SearchService.java
@@ -1,7 +1,25 @@
 package com.kgg.kart.search.service;
 
+import com.kgg.kart.common.dto.CommonResponse;
+import com.kgg.kart.search.domain.Search;
+import com.kgg.kart.search.dto.SearchRequest;
+import com.kgg.kart.search.repository.SearchRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SearchService {
+    private final SearchRepository searchRepository;
+    private CommonResponse commonResponse=new CommonResponse();
+    public void saveInfo(String nickname, String decode){
+        Search search=new Search();
+        search.setNickname(nickname);
+        search.setDecode(decode);
+        searchRepository.save(search);
+    }
+    public CommonResponse sendNexon(SearchRequest searchRequest){
+
+        return commonResponse;
+    }
 }
