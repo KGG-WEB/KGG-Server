3b09d50525b687a408def924bf0ecc93a471a21e
myeongju-kim <mjoo1106@naver.com>
2022-10-26 10:39:53 +0900

    feat: create search table

---
+++ src/main/java/com/kgg/kart/search/domain/Search.java
@@ -0,0 +1,17 @@
+package com.kgg.kart.search.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Search {
+    @Id
+    @GeneratedValue
+    private Long mapId;
+    private String nickname;
+    private String decode;
+}
